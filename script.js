2ef9ec1e58edeaa265aae7b935d1c80a108671c5
Bruna Polonio <polonio.bruna@gmail.com>
2020-06-05 17:45:58 -0300

    definir aleatoridade da grutinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -47,7 +47,6 @@ function iniciarJogo(){
 	if (snake[0].y > 15 * box && direction == "down") snake[0].y=0;
 	if (snake[0].y < 0 && direction == "up") snake[0].y = 16* box;
 
-
 	criarBG();
 	criarCobrinha();
 	drawFood();
@@ -60,8 +59,13 @@ function iniciarJogo(){
 	if (direction == "up") snakeY -= box;
 	if (direction == "down") snakeY += box;
 
-	snake.pop();
-	
+	if(snakeX != food.x || snakeY != food.y){
+		snake.pop();
+	}
+	else{food.x = Math.floor(Math.random() * 15 + 1) * box
+		food.y = Math.floor(Math.random() * 15 + 1) * box
+	}
+
 	let newHead = {
 		x: snakeX,
 		y: snakeY
